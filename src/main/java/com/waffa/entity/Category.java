e0a95c892fe7e84a4d7bcc99f5d7e5e30223462e
unknown <alaa.mezian.mail@gmail.com>
2018-05-10 17:17:16 +0300

    data base configuration

--- src/main/java/com/waffa/entity/Category.java
+++ src/main/java/com/waffa/entity/Category.java
@@ -12,8 +12,6 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
-import lombok.Getter;
-import lombok.Setter;
 
 @Entity
 @Table(name = "category")
--- src/test/com/waffa/test/JpaTest.java
+++
@@ -1,5 +0,0 @@
-package com.waffa.test;
-
-public class JpaTest {
-
-}
